47548e5f3c1afc90dce42fd6b2d87a651f2e89e3
SergPaderov <sergeipaderov@gmail.com>
2018-01-25 22:43:25 -0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React from "react";
-import NewsPost from "./NewsPost";
-import "./App.css";
+import React from 'react';
+import NewsPost from './NewsPost';
+import './App.css';
 
 class App extends React.Component {
-  state = {
-    newsInput: "",
-    news: []
-  };
-  handleChange = e => {
-    this.setState({ newsInput: e.target.value });
-  };
-  handleNewPost = e => {
-    let { value, news } = this.state;
+    state = {
+        newsInput: "",
+        news: []
+    };
+    handleChange = e => {
+        this.setState({ newsInput: e.target.value });
+    };
+    handleNewPost = e => {
+        
+        let { value, news } = this.state;
 
-    this.setState({
-      value: "",
-      news: [...news, value]
-    });
-  };
-  render() {
-    let { value, news } = this.state;
-    return (
-      <div>
-        <div className="wrapper App">
-          <input
-            type="text"
-            className="comment-input"
-            placeholder=""
-            onChange={this.handleChange}
-            value={value}
-          />
-          <button className={"button"} onClick={this.handleNewPost}>
-            Push
-          </button>
-        </div>
-        <NewsPost news={news} />
-      </div>
-    );
-  }
+        //const _news = [...news, value];
+        
+        this.setState({
+            value: "",
+            news: [...news, value]
+        })
+    };
+    render() {
+        let { value, news } = this.state;
+        return (
+            <div>
+                <div className="wrapper App">
+                    <input
+                        type="text"
+                        className="comment-input"
+                        placeholder=""
+                        onChange={this.handleChange}
+                        value={value}
+                    />
+                    <button
+                        className={"button"}
+                        onClick={this.handleNewPost}
+                    >
+                        Push
+                    </button>
+                </div>
+                <NewsPost news={news} />                
+            </div>
+        );
+    }
 }
 
 export default App;
